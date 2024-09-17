eaa97085340c601bf64f4e35f38206d30855a889
rizflash <rizky1234rzk@gmail.com>
2024-09-17 19:27:50 +0700

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'I Have Something 4u'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
